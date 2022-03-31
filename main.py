22010883a8e8a27eb9231d37222de716fa5d5fcd
jayashree-j <97395018+jayashree-jayakumar@users.noreply.github.com>
2022-03-31 09:58:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+ran_int=random.randint(0,1)
+if (ran_int==1):
+    print("Heads")
+else:
+    print("Tails")
+
+
+
+
+
+
+
+
